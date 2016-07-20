131a2740b4068d0a315f14aefe005bb4615098d0
Jacob Shorty <jakeshorty@gmail.com>
2016-07-20 13:29:00 -0400

    Allow subject in ScheduleMailing

--- lib/silverpop/engage.rb
+++ lib/silverpop/engage.rb
@@ -252,8 +252,8 @@ module Silverpop
 
     # Currently only supports using existing template content in the top-level folder.
     # Consult API documentation and tweak XML to handle those additional options.
-    def schedule_mailing(template_id:, list_id:, name:, use_html: true, visibility: 1, suppression_list_ids: [], custom_opt_out: nil)
-      xml = query(xml_schedule_mailing(template_id, list_id, name, use_html, visibility, suppression_list_ids, custom_opt_out))
+    def schedule_mailing(template_id:, list_id:, name:, use_html: true, subject: nil, visibility: 1, suppression_list_ids: [], custom_opt_out: nil)
+      xml = query(xml_schedule_mailing(template_id, list_id, name, use_html, subject, visibility, suppression_list_ids, custom_opt_out))
       Nokogiri::XML(xml).at('MAILING_ID').text
     end
 
@@ -712,7 +712,7 @@ module Silverpop
       "<SYNC_FIELDS>#{field_names.map { |n| "<SYNC_FIELD><NAME>#{n}</NAME></SYNC_FIELD>" }.join}</SYNC_FIELDS>"
     end
 
-    def xml_schedule_mailing(template_id, list_id, name, use_html, visibility, suppression_list_ids, custom_opt_out)
+    def xml_schedule_mailing(template_id, list_id, name, use_html, subject, visibility, suppression_list_ids, custom_opt_out)
       sids = suppression_list_ids.map do |id|
         "<SUPPRESSION_LIST_ID>#{id}</SUPPRESSION_LIST_ID>"
       end
@@ -726,6 +726,7 @@ module Silverpop
             #{use_html ? "<SEND_HTML/><SEND_TEXT/>": "<SEND_TEXT/>"}
             #{sids.any? ? "<SUPPRESSION_LISTS>#{sids.join}</SUPPRESSION_LISTS>" : nil}
             #{custom_opt_out ? "<CUSTOM_OPT_OUT>#{custom_opt_out}</CUSTOM_OPT_OUT>" : nil}
+            #{subject ? "<SUBJECT>#{subject}</SUBJECT>" : nil}
             <VISIBILITY>#{visibility}</VISIBILITY>
           </ScheduleMailing>
         XML
